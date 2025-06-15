40d9633f339250396f9babb610327dcdcf92706c
Tarique Mosharraf <applandsys@gmail.com>
2025-06-16 00:56:10 +0600

    Feat: Blog List blog Detail

---
+++ src/Interfaces/PostInterfaces.ts
@@ -0,0 +1,29 @@
+export interface Category {
+    name: string;
+    slug: string;
+}
+
+export interface Post {
+    slug: string;
+    title: string;
+    content: string;
+    excerpt: string;
+    featuredImage: {
+        node: {
+            mediaDetails: {
+                sizes: {
+                    sourceUrl: string;
+                    width: number;
+                    height: number;
+                }[];
+            };
+        };
+    };
+    categories: {
+        nodes: Category[];
+    };
+}
+
+export interface PostDetailProps {
+    params: { slug: string };
+}
