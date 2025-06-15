7cc31f55a6d043219c335f499cad78a139bdf9a4
Tarique Mosharraf <applandsys@gmail.com>
2025-06-16 03:03:49 +0600

    Feat: even confi9g

---
+++ next.config.js
@@ -0,0 +1,19 @@
+
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'wp.mentorofcure.com',
+                // port: '', // Optional, if your domain uses a specific port
+                // pathname: '/images/**', // Optional, if images are under a specific path
+            },
+            {
+                protocol: 'https',
+                hostname: 'wp.mentorofcure.com',
+            },
+        ],
+    },
+};
+
+export default nextConfig;
